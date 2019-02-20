6fe812186d617dca37063b5174b60c061ae99d63
Matthias Haehnel <matthias.haehnel@sixt.com>
2019-02-20 10:34:21 +0100

    corrected bluecat product name

--- lib/smart_proxy_dhcp_bluecat/module_loader.rb
+++ lib/smart_proxy_dhcp_bluecat/module_loader.rb
@@ -1,6 +1,6 @@
 class ::Proxy::DHCP::BlueCat::ModuleLoader < ::Proxy::DefaultModuleLoader
   def log_provider_settings(settings)
     super(settings)
-    logger.warn('http is used for connection to BlueCat appliance') if settings[:scheme] != 'https'
+    logger.warn('http is used for connection to BlueCat address manager') if settings[:scheme] != 'https'
   end
 end
