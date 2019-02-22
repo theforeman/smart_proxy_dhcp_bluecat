043a547c69eaf85cf5a8a17cef26b6cf6bcf53ee
Matthias Haehnel <matthias.haehnel@sixt.com>
2019-02-22 16:02:38 +0100

    test data added

--- test/plugin_configuration_test.rb
+++ test/plugin_configuration_test.rb
@@ -9,23 +9,33 @@ require 'smart_proxy_dhcp_bluecat/dhcp_bluecat_main'
 
 class BluecatDhcpProductionWiringTest < Test::Unit::TestCase
   def setup
-    @settings = { :username => 'user',
-                  :password => 'password',
-                  :server => '10.10.10.10',
-                  :scheme => 'https',
+    @settings = { :scheme => 'https',
                   :verify => true,
-                  :subnets => ['1.1.1.0/255.255.255.0'] }
+                  :host => '10.10.10.10',
+                  :parent_block => 242_435,
+                  :view_name => 'default',
+                  :config_name => 'Sixt',
+                  :config_id => 100_881,
+                  :server_id => 100_901,
+                  :username => 'apiuser',
+                  :password => 'password' }
+
     @container = ::Proxy::DependencyInjection::Container.new
     Proxy::DHCP::Bluecat::PluginConfiguration.new.load_dependency_injection_wirings(@container, @settings)
   end
 
   def test_connection_initialization
     connection = @container.get_dependency(:connection)
-    assert_equal '10.10.10.10', connection.host
-    assert_equal 'user', connection.username
-    assert_equal 'password', connection.password
     assert_equal 'https', connection.scheme
     assert_equal true, connection.verify
+    assert_equal '10.10.10.10', connection.host
+    assert_equal 242_435, connection.parent_block
+    assert_equal 'default', connection.view_name
+    assert_equal 'Sixt', connection.config_name
+    assert_equal 100_881, connection.config_id
+    assert_equal 100_901, connection.server_id
+    assert_equal 'user', connection.username
+    assert_equal 'password', connection.password
   end
 
   def test_provider
